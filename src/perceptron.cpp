dc41286ce8a0645189322f6cd7dee55d90419831
Creator <52988770+OriginatorX@users.noreply.github.com>
2021-03-14 12:20:04 +1000

    Update perceptron.cpp

--- src/perceptron.cpp
+++ src/perceptron.cpp
@@ -10,14 +10,13 @@ Perceptron::Perceptron()
 	, rows(2) 
 	{
 
-	inNeurons = new double[inNeuronsLength];
+	inNeurons       = new double[inNeuronsLength];
 	synapsesWeights = new double[inNeuronsLength];
-	learnPattern = new double* [columns];
-	trueOutValue = new double[columns];
+	learnPattern    = new double* [columns];
+	trueOutValue    = new double[columns];
 
 	for (size_t item{ 0 }; item < columns; item++) 
 		learnPattern[item] = new double[rows];
-	
 
 	std::cout << "Input learnPatterns:\n";
 
@@ -31,9 +30,8 @@ Perceptron::Perceptron()
 
 	std::cout << "learnPatterns:\n";
 	for (size_t i{ 0 }; i < columns; i++) {
-		for (size_t j{ 0 }; j < rows; j++) {
+		for (size_t j{ 0 }; j < rows; j++)
 			std::cout << learnPattern[i][j] << " ";
-		}
 		std::cout << "\n";
 	}
 
@@ -46,19 +44,16 @@ Perceptron::Perceptron()
 	}
 
 	std::cout << "trueOutValue:\n";
-	for (size_t item{ 0 }; item < columns; item++) {
-		std::cout << trueOutValue[item] << " ";
-	}
+	for (size_t item{ 0 }; item < columns; item++)
+		std::cout << trueOutValue[item] << " ";	
 	std::cout << "\n";
 
-	for (size_t item{ 0 }; item < inNeuronsLength; item++) {
+	for (size_t item{ 0 }; item < inNeuronsLength; item++) 
 		synapsesWeights[item] = getRangeRandom(0, 1) * 0.2 + 0.1;
-	}
 
 	std::cout << "Weights:\n";
-	for (size_t item{ 0 }; item < inNeuronsLength; item++) {
-		std::cout << synapsesWeights[item] << "\n";
-	}
+	for (size_t item{ 0 }; item < inNeuronsLength; item++) 
+		std::cout << synapsesWeights[item] << "\n";	
 	std::cout << "\n";
 }
 
@@ -80,7 +75,6 @@ Perceptron::~Perceptron() {
 }
 
 double Perceptron::getRangeRandom(int min, int max) {
-	
 	std::random_device rand;
 	std::default_random_engine gen(rand());
 	std::uniform_int_distribution<> dis(min, max);
@@ -88,21 +82,21 @@ double Perceptron::getRangeRandom(int min, int max) {
 }
 
 size_t Perceptron::evolution() {
-	double gError{ 0 };
-	size_t count{};
+	double gError = 0;
+	size_t count  = 0;
 
 	do {
 		gError = 0;
 		count++;
-		for (size_t p{ 0 }; p < columns; p++) {
-			for (size_t i{ 0 }; i < inNeuronsLength; i++)
+		for (size_t p = 0 ; p < columns; p++) {
+			for (size_t i = 0; i < inNeuronsLength; i++)
 				inNeurons[i] = learnPattern[p][i];
 			valueOfOutNeuron();
 
 			double error = trueOutValue[p] - outNeuron;
 			gError += abs(error);
 
-			for (size_t item{ 0 }; item < inNeuronsLength; item++) {
+			for (size_t item = 0; item < inNeuronsLength; item++) {
 				synapsesWeights[item] += 0.1 * error * inNeurons[item];
 			}
 		}
@@ -117,7 +111,7 @@ void Perceptron::valueOfOutNeuron() {
 	
 	double theta{ 0.5 };
 
-	for (size_t item{ 0 }; item < inNeuronsLength; item++) {
+	for (size_t item = 0 ; item < inNeuronsLength; item++) {
 		outNeuron += inNeurons[item] * synapsesWeights[item];
 	}
 	if (outNeuron > theta) outNeuron = 1;
@@ -128,8 +122,8 @@ void Perceptron::start() {
 	std::cout << evolution() << "\n";
 	std::cout << "Result:\n";
 
-	for (size_t p{ 0 }; p < columns; p++) {
-		for (size_t i{ 0 }; i < inNeuronsLength; i++)
+	for (size_t p = 0; p < columns; p++) {
+		for (size_t i = 0; i < inNeuronsLength; i++)
 			inNeurons[i] = learnPattern[p][i];
 		valueOfOutNeuron();
 		std::cout << "Out of neuron: " << outNeuron << ", ";
