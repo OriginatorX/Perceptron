f50dd3f22a4dc72b2dd903f69e74b32d22076afc
Nik Fetisoff <52988770+OriginatorX@users.noreply.github.com>
2021-08-23 13:44:30 +1000

    Update perceptron.hpp

--- inc/perceptron.hpp
+++ inc/perceptron.hpp
@@ -7,22 +7,22 @@ public:
 	Perceptron(const Random& rand);
 	~Perceptron();
 public:
-	void   valueOfOutNeuron();	
-	size_t evolution();
-	void   start();
+	void     valueOfOutNeuron();	
+	size_t   evolution();
+	void     start();
 private:
-	Random 		random_;
+	Random 	 random_;
 
-	size_t 	 	columnsNumber_;
-	size_t 		rowsNumber_;
+	size_t 	 columnsNumber_;
+	size_t 	 rowsNumber_;
 
-	double*  	inputNeurons_;
-	double   	outNeuron_;	
-	double*  	synapsesWeights_;	
-	double** 	learnPattern_;	
-	double*  	trueOutValue_;
+	double*  inputNeurons_;
+	double   outNeuron_;	
+	double*  synapsesWeights_;	
+	double** learnPattern_;	
+	double*  trueOutValue_;
 	
-	size_t 		inputNeuronsNumber_;
+	size_t 	 inputNeuronsNumber_;
 public:
 	Perceptron(const Perceptron&) = delete;
 	Perceptron& operator=(const Perceptron&) = delete;
