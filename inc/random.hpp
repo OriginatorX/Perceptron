e07e186af192ded1c401b5ca7cfa9eb3b1d952d9
Nik Fetisoff <52988770+OriginatorX@users.noreply.github.com>
2021-08-23 13:45:33 +1000

    Update random.hpp

--- inc/random.hpp
+++ inc/random.hpp
@@ -4,22 +4,22 @@
 
 class Random {
 public:
-	using integer_rand	= std::uniform_int_distribution<int32_t>;
-	using double_rand	= std::uniform_real_distribution<double_t>;
+	using integer_rand  = std::uniform_int_distribution<int32_t>;
+	using double_rand   = std::uniform_real_distribution<double_t>;
 public:
 	Random();
 public:
-	const int		operator() ();
+	const int     operator() ();
 
-	const int		operator() (int32_t lower, int32_t upper);
+	const int     operator() (int32_t lower, int32_t upper);
 
-	const double	operator() (double_t lower, double_t upper);
+	const double  operator() (double_t lower, double_t upper);
 
-	const int   	IntContent()	const;
-	const double  	DoubleContent()	const;
+	const int     IntContent()	const;
+	const double  DoubleContent()	const;
 private: 
 	int 	randomINumber_;
 	double	randomDNumber_;
 
-	std::default_random_engine	engine_;
+	std::default_random_engine engine_;
 };
